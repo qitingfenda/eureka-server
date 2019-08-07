1e9b5b9b2ae857fdc90605091b5bb82086afa3b0
yhc <2264937039@qq.com>
2019-08-07 16:03:15 +0800

    Initial commit

---
+++ src/main/java/com/djcps/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.djcps.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author yhc
+ * @date 2019/7/29 15:24
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
